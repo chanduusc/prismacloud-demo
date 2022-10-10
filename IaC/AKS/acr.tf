2b44618ed8284a2095cde4b80eec2896fa1cf89a
Milan Krstic <5746174+mkrle@users.noreply.github.com>
2022-10-10 14:29:12 +0200

    Add ACR

---
+++ IaC/AKS/acr.tf
@@ -0,0 +1,26 @@
+resource "azurerm_container_registry" "acr" {
+  count               = var.create_acr ? 1 : 0
+  name                = var.image_repository
+  resource_group_name = azurerm_resource_group.rg[0].name
+  sku                 = "Basic"
+  location            = var.region
+}
+
+resource "azurerm_role_assignment" "sp_acr_role" {
+  count                = var.create_acr ? 1 : 0
+  scope                = azurerm_container_registry.acr[count.index].id
+  role_definition_name = "acrpush"
+  principal_id         = azuread_service_principal.sp[count.index].id
+}
+
+data "azurerm_kubernetes_cluster" "aks" {
+  resource_group_name = azurerm_kubernetes_cluster.aks_cluster.resource_group_name
+  name                = azurerm_kubernetes_cluster.aks_cluster.name
+}
+
+resource "azurerm_role_assignment" "aks_acr_role" {
+  count                            = var.create_acr ? 1 : 0
+  scope                            = azurerm_container_registry.acr[count.index].id
+  role_definition_name             = "acrpull"
+  principal_id                     = data.azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
