790fee029734f4bebc6d451ac8ad82de9426112e
Milan Krstic <5746174+mkrle@users.noreply.github.com>
2023-03-30 13:54:02 +0200

    Add S3 bucket policy.

--- IaC/EKS/s3.tf
+++ IaC/EKS/s3.tf
@@ -23,3 +23,29 @@ resource "aws_s3_bucket_public_access_block" "pab" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_policy" "allow_upload" {
+  bucket = aws_s3_bucket.s3b.bucket
+  policy = data.aws_iam_policy_document.allow_upload.json
+}
+
+data "aws_iam_policy_document" "allow_upload" {
+  statement {
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListMultipartUploadParts",
+      "s3:PutObject",
+      "s3:PutObjectAcl"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [module.eks.eks_managed_node_groups.one.iam_role_arn]
+    }
+    resources = [
+      aws_s3_bucket.s3b.arn,
+      "${aws_s3_bucket.s3b.arn}/*"
+    ]
+  }
+}
