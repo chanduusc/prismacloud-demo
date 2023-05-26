0899d5eeb85c01f2440f94535483e4e01ebcbb0b
Milan Krstic <5746174+mkrle@users.noreply.github.com>
2023-05-26 21:17:34 +0200

    EKS version to variable.

--- IaC/EKS/eks.tf
+++ IaC/EKS/eks.tf
@@ -18,7 +18,7 @@ module "eks" {
   version = "~> 18.0"
 
   cluster_name    = local.cluster_name_full
-  cluster_version = "1.23"
+  cluster_version = var.cluster_version
 
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
