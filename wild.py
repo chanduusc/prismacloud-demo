8855822ec15b59cb1b2857dd86e7c8037205f1b7
chcheruk <chandra.cheruku@gmail.com>
2024-02-14 14:01:47 -0800

    Demo Name Change

--- wild.py
+++ wild.py
@@ -31,11 +31,7 @@ elif 'azure' in cloud_provider:
     with open('/plz_del/FritzFrog/clia-lab-search-results-02.22.2023-19_43_13.csv', "rb") as data:
             blob_client = container_client.upload_blob(name=unique_hipaa_filename, data=data) 
 else:
-    s3 = boto3.resource('s3')
-    s3.meta.client.upload_file('/plz_del/FritzFrog/001eb377f0452060012124cb214f658754c7488ccb82e23ec56b2f45a636c859', 'cnappdemo' , unique_malware_filename)
-    s3.meta.client.upload_file('/plz_del/FritzFrog/10-MB-Test.docx', 'cnappdemo' , unique_sensitive_filename, ExtraArgs={'ACL':'public-read'})
-    s3.meta.client.upload_file('/plz_del/FritzFrog/clia-lab-search-results-02.22.2023-19_43_13.csv', 'cnappdemo' , unique_hipaa_filename, ExtraArgs={'ACL':'public-read'})
-    
+    next
 hostName = "0.0.0.0"
 serverPort = 8080
 class MyServer(BaseHTTPRequestHandler):
@@ -51,7 +47,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<p>Path: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<p>Requestor: %s</p>" % self.request.getpeername()[0], "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>Demo Server - Code to Cloud Attack Paths</p>", "utf-8"))
+        self.wfile.write(bytes("<p>Demo Server - Code to Cloud Attack Path</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
     def do_OPTIONS(self):
         self.send_response(200, "ok")
